4e86d0c2325f42e3fc0d952eaef4b6b2e1c1eb72
zqca <514564149@qq.com>
2021-03-08 10:25:01 +0800

    回文串切割次数

---
+++ 132.minCut.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int minCut(string s) {
+
+}
+
+int main() {
+    string s = "aab";
+    cout << minCut(s) << endl;
+
+    return 0;
+}
