a7d500b34e1f3836d80dec0a3f8d0979601de7ca
zqca <514564149@qq.com>
2021-04-02 21:53:08 +0800

    暴力＋dp

--- 17.21.trap.cpp
+++ 17.21.trap.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-int trap(vector<int>& height) {
+int trap1(vector<int>& height) {
+    //暴力
     int res = 0;
     int l, r;
 
@@ -14,6 +15,34 @@ int trap(vector<int>& height) {
         for(int j = i; j < height.size(); j++) {r = max(height[j], r);}
         //当前位置能接到的数字
         res += min(l,r) - height[i];
+    }
+    return res;
+
+}
+
+int trap(vector<int>& height) {
+    //动态规划
+    
+    int res = 0;
+    int len = height.size();
+    if(len == 0) return 0;
+    vector<int> maxleft(len), maxright(len);
+    //本元素及其左侧、右侧的最大高度
+
+    maxleft[0] = height[0];
+    maxright[len - 1] = height[len - 1];
+
+    for(int i = 1; i < len; i++){
+        maxleft[i] = max(maxleft[i - 1], height[i]);
+    }
+
+    for(int i = len - 2; i >= 0; i--){
+        maxright[i] = max(maxright[i + 1], height[i]);
+    }
+
+    for(int i = 1; i < height.size(); i++) {
+        //当前位置能接到的数字
+        res += min(maxleft[i], maxright[i]) - height[i];
     }
     return res;
 
