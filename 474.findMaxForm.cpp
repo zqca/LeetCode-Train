700e0d81aff31c2e2a2864a04cb16ac08198e3d9
zqca <514564149@qq.com>
2021-06-06 10:26:50 +0800

    动态规划，01背包

--- 474.findMaxForm.cpp
+++ 474.findMaxForm.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 
 int findMaxForm(vector<string>& strs, int m, int n) {
-
-    vector<int> num0(strs.size(), 0);//m
-    vector<int> num1(strs.size(), 0);//n
+    int len = strs.size();
+    vector<int> num0(len, 0);//m
+    vector<int> num1(len, 0);//n
     
-    for(int i = 0; i < strs.size(); ++i) {//i是每一个字符串
+    for(int i = 0; i < len; ++i) {//i是每一个字符串
         for(int k = 0; k < strs[i].length(); ++k) {
             if(strs[i][k] == '0') num0[i]++;
             else num1[i]++;
@@ -18,7 +18,8 @@ int findMaxForm(vector<string>& strs, int m, int n) {
 
     vector<vector<int> > dp(m + 1, vector<int>(n + 1, 0));
     //dp[i][j]存放 0最多为i个，1最多j个的子集的大小
-    for(int k = 0; k < strs.size(); k++) {
+    for(int k = 0; k < len; k++) {
+        //考虑当前元素，更新数组
         for(int i = m; i >=  num0[k]; i--) {
             for(int j = n; j >= num1[k]; j--) {
                 dp[i][j] = max(dp[i][j], dp[i - num0[k]][j - num1[k]] + 1);
