8dd8096c3af75b681c72c597e124c0e064f2688d
zqca <514564149@qq.com>
2021-06-24 22:00:16 +0800

    hard,自己设计一个简单的映射关系

--- 149.cpp
+++ 149.cpp
@@ -1,54 +1,40 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 
 using namespace std;
 
 class Solution {
 public:
-    int gcd(int a, int b) {
-        return b ? gcd(b, a % b) : a;
-    }
-
     int maxPoints(vector<vector<int>>& points) {
-        int n = points.size();
-        if (n <= 2) {
-            return n;
-        }
-        int ret = 0;
-        for (int i = 0; i < n; i++) {
-            if (ret >= n - i || ret > n / 2) {
-                break;
-            }
-            unordered_map<int, int> mp;
-            for (int j = i + 1; j < n; j++) {
-                int x = points[i][0] - points[j][0];
-                int y = points[i][1] - points[j][1];
-                if (x == 0) {
-                    y = 1;
-                } else if (y == 0) {
-                    x = 1;
-                } else {
-                    if (y < 0) {
-                        x = -x;
-                        y = -y;
-                    }
-                    int gcdXY = gcd(abs(x), abs(y));
-                    x /= gcdXY, y /= gcdXY;
+        int ans = 1;
+
+        for(int i = 0; i < points.size(); i ++){
+            unordered_map<long long, int> map;
+            for(int j = i + 1; j < points.size(); j ++){    
+                // 计算deltaX deltaY
+                int x = points[j][0] - points[i][0];
+                int y = points[j][1] - points[i][1];
+
+                if(x == 0){ // 斜率不存在的情况
+                    map[1e18 + points[j][0]] ++;   // 1e18 = 1e9 * 1e9
+                }else{
+                    double k = (double) y / x;
+                    double b = points[j][1] - k * points[j][0];
+                    long long tmp = k * 1e9 + b;    // 合成新的哈希值
+                    ++ map[tmp];
                 }
-                mp[y + x * 20001]++;
             }
-            int maxn = 0;
-            for (auto& [_, num] : mp) {
-                maxn = max(maxn, num + 1);
-            }
-            ret = max(ret, maxn);
+            // 统计答案
+            for(auto &[i, j] : map)
+                ans = max(ans, j + 1);
         }
-        return ret;
+        return ans;
     }
 };
 /*
-作者：LeetCode-Solution
-链接：https://leetcode-cn.com/problems/max-points-on-a-line/solution/zhi-xian-shang-zui-duo-de-dian-shu-by-le-tq8f/
+作者：Raymond_YP
+链接：https://leetcode-cn.com/problems/max-points-on-a-line/solution/c-zhi-xian-shang-zui-duo-de-dian-shu-by-wvol4/
 来源：力扣（LeetCode）
 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 */
