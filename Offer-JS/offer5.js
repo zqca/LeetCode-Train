772fab90b91f8c7ec85ef105c5867b4e4c091028
zqca <514564149@qq.com>
2021-06-27 19:47:01 +0800

    string转数组split的使用，数组转string join的使用

---
+++ Offer-JS/offer5.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var replaceSpace = function(s) {
+    let arr = s.split("");
+    let oldLen = arr.length;
+    let spaceCount = 0;
+    for (let i = 0; i < oldLen; i++) {
+        if (arr[i] === ' ') spaceCount++;
+    }
+    arr.length += spaceCount * 2;
+
+    for (let i = oldLen - 1, j = arr.length - 1; i >= 0; i--, j--) {
+        if (arr[i] !== ' ') arr[j] = arr[i];
+        else {
+            arr[j - 2] = '%';
+            arr[j - 1] = '2';
+            arr[j] = '0';
+            j -= 2;
+        }
+    }
+    return arr.join('');
+};
+
+let ans = replaceSpace('ha s');
+console.log(ans);
+
