428ca759206f70c9e5d88838ba47152354b2b476
zqca <514564149@qq.com>
2021-07-12 11:52:42 +0800

    快慢指针，原地改造；慢指针确定填入的位置，快指针寻找奇数； 双指针也可以做。

--- Offer-JS/offer21.js
+++ Offer-JS/offer21.js
@@ -22,7 +22,7 @@ var exchange0 = function(nums) {
  * @return {number[]}
  * 快慢指针，原地改造；慢指针确定填入的位置，快指针寻找奇数
  */
-var exchange = function(nums) {
+var exchange1 = function(nums) {
     let len = nums.length;
     let slow = 0, fast = 0;
     let tmp;
@@ -37,4 +37,29 @@ var exchange = function(nums) {
     }
 
     return nums;
+};
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ * 双指针
+ */
+var exchange = function(nums) {
+    let left = 0, right = nums.length - 1;
+    let tmp;
+    while(left < right){
+        if(nums[left] % 2){
+            //奇数
+            ++left;
+        }
+        else{
+            //偶数，先交换，不动left的位置
+            tmp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = tmp;
+            --right;
+        }
+    }
+    return nums;
+
 };
