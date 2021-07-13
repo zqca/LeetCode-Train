112d1e0173fedfc06870e71cc6e635d5248fe175
zqca <514564149@qq.com>
2021-07-13 20:55:45 +0800

    反转链表

---
+++ Offer-JS/offer24.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(head === null) {
+        return head;
+    }
+
+    let pre = null;
+    let cur = head;
+    let nxt;
+    while(cur){
+        nxt = cur.next;
+        cur.next = pre;
+        pre = cur;
+        cur = nxt;
+    }
+    return pre;
+};
