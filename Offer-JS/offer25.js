caaba66f26ff0960270e88262abbf43afcafdf61
zqca <514564149@qq.com>
2021-07-14 09:55:02 +0800

    合并排序后的链表,不需要创建新node;递归也行。

--- Offer-JS/offer25.js
+++ Offer-JS/offer25.js
@@ -9,18 +9,19 @@
  * @param {ListNode} l1
  * @param {ListNode} l2
  * @return {ListNode}
+ * 迭代法
  */
 
-var mergeTwoLists = function(l1, l2) {
+var mergeTwoLists0 = function(l1, l2) {
     let ans = new ListNode(-1), cur = ans;
     while(l1 !== null && l2 !== null){
         let tmp;
         if(l1.val < l2.val){
-            tmp = new ListNode(l1.val);
+            tmp = l1;
             l1 = l1.next;
         }
         else{
-            tmp = new ListNode(l2.val);
+            tmp = l2;
             l2 = l2.next;
         }
         cur.next = tmp;
@@ -35,4 +36,30 @@ var mergeTwoLists = function(l1, l2) {
     }
     return ans.next;
 
+};
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ * 递归法
+ */
+
+var mergeTwoLists = function(l1, l2) {
+
+    if(l2 === null){
+        return l1;
+    }
+    if(l1 === null) {
+        return l2;
+    }
+
+    if(l1.val < l2.val){
+        l1.next = mergeTwoLists(l1.next, l2);
+        return l1;
+    }
+    else{
+        l2.next = mergeTwoLists(l1, l2.next);
+        return l2;
+    }
 };
