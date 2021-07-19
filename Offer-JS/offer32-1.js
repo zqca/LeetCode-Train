2f7d7c3ec27005fd24e733bc01ec6692f30040e5
zqca <514564149@qq.com>
2021-07-19 20:08:36 +0800

    层次遍历

---
+++ Offer-JS/offer32-1.js
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ * 层次遍历
+ */
+var levelOrder = function(root) {
+    let ans = [];
+    let que = [];
+    if(root){
+        que.push(root);
+    }
+    else {
+        return ans;
+    }
+    while(que.length){
+        let len = que.length;
+        for(let i = 0; i < len; ++i){
+            let node = que[0];
+            que.shift();
+            ans.push(node.val);
+            if(node.left){
+                que.push(node.left);
+            }
+            if(node.right){
+                que.push(node.right);
+            }
+        }
+    }
+    return ans;
+};
